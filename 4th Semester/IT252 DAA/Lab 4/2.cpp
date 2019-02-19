2e711c47b7c809b4bfe2839e1b0d531843ff545e
Bharath <lkcbharath@gmail.com>
2019-02-20 02:41:33 +0530

    Frontend

--- 4th Semester/IT252 DAA/Lab 4/2.cpp
+++ 4th Semester/IT252 DAA/Lab 4/2.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
-int majority(int* arr){
+int FindMost(int *A, int startIndex, int endIndex)
+{  // input array A
+	int x,y;
 
-	return 0;
+	if (startIndex == endIndex)  // base case
+	        return A[startIndex]; 
+	
+	x = FindMost(A, startIndex, (startIndex + endIndex - 1)/2);
+	y = FindMost(A, (startIndex + endIndex - 1)/2 + 1, endIndex);
+
+	if (x == -1 && y == -1) 
+	    return -1;
+	else if (x == -1 && y != -1) 
+	    return y;
+	else if (x != -1 && y == -1) 
+	    return x;
+	else if (x != y) 
+	    return -1;
+	else 
+		return x;
+
 }
 
 int main(){
@@ -13,7 +31,11 @@ int main(){
 
 	n = 8;
 
-	int a[] = {1,5,3,1,2,3,1,23};
+	int a[] = {1,1,3,1,2,3,1,23	};
+
+	int result = FindMost(a,0,n-1);
+
+	cout << result << endl;
 
 	return 0;
 }
