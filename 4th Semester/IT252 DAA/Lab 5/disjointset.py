fa3e061fa1772434e03ea87df2a4722692db1ca7
student <student@itadmin.nitk.ac.in>
2019-02-21 16:33:41 +0530

    DAA Lab 5 #1

--- 4th Semester/IT252 DAA/Lab 5/disjointset.py
+++ 4th Semester/IT252 DAA/Lab 5/disjointset.py
@@ -16,11 +16,16 @@ class DisjointSet:
 		self.array[value] = InsertSet
 
 	def findSet(self,x):
-		if x.parent!=x:
-			y = findSet(x.parent)
-			x.parent = y
+		self.printSets
+		x_set = self.array[x]
+
+		if x_set.parent!=x_set.value:
+			
+			y = self.findSet(x_set.parent)
+			x_set.parent = y.value
 			return y
-		return x
+		
+		return x_set
 
 	def union(self,x,y):
 
@@ -28,21 +33,27 @@ class DisjointSet:
 		ry = self.findSet(y)
 
 		if (rx.rank > ry.rank):
-			ry.parent = rx
+			ry.parent = rx.value
 		elif (rx.rank < ry.rank):
-			rx.parent = ry
+			rx.parent = ry.value
 		else:
-			ry.parent = rx
+			ry.parent = rx.value
 			rx.rank += 1
 
 
 	def printSets(self):
 		for i in range(self.size):
-			print(self.array[i].parent)
+			print(self.array[i].parent, end = ' ')
+		print("")
 
 def main():
-	ds = DisjointSet(1)
-	ds.makeSet(0,0)
+	ds = DisjointSet(5)
+	ds.makeSet(1,2)
+	ds.makeSet(0,2)
+	ds.makeSet(2,2)
+	ds.makeSet(3,4)
+	ds.makeSet(4,4)
+	ds.union(2,3)
 	ds.printSets()
 
 if __name__ == '__main__':
