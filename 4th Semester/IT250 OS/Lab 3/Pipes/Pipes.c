b10436b25e9a97e224717fd225a2fbb077e629c2
student <student@itadmin.nitk.ac.in>
2019-03-06 14:17:44 +0530

    Pipes

--- 4th Semester/IT250 OS/Lab 3/Pipes/Pipes.c
+++ 4th Semester/IT250 OS/Lab 3/Pipes/Pipes.c
@@ -11,10 +11,13 @@ char* msg3 = "hello, world #3";
 int main() 
 { 
 	char inbuf[MSGSIZE]; 
-	int p[2], pid, nbytes; 
+	int p[2], q[2], pid, nbytes; 
 
 	if (pipe(p) < 0) 
 		exit(1); 
+	
+	if (pipe(q) < 0) 
+		exit(1);
 
 	/* continued */
 	if ((pid = fork()) > 0) { 
@@ -22,20 +25,33 @@ int main()
 		write(p[1], msg1, MSGSIZE); 
 		write(p[1], msg2, MSGSIZE);
 		write(p[1], msg3, MSGSIZE);
-		printf("Parent Process done\n");
+		printf("Finished writing in Parent Process\n");
 		// Adding this line will 
 		// not hang the program 
 		close(p[1]); 
+		close(q[1]);
+		
 		// wait(NULL); 
+		while ((nbytes = read(q[0], inbuf, MSGSIZE)) > 0) 
+			printf("Message from child: % s\n", inbuf); 
+		if (nbytes != 0) 
+			exit(2); 
+		 
+		printf("Finished reading in Parent Process\n");
 	} 
 
 	else { 
 		printf("Child Process\n");
 		// Adding this line will 
-		// not hang the program 
+		// not hang the program
+		write(q[1], msg1, MSGSIZE); 
+		write(q[1], msg2, MSGSIZE);
+		write(q[1], msg3, MSGSIZE); 
+		printf("Finished writing in Child Process\n");
+		close(q[1]); 
 		close(p[1]); 
 		while ((nbytes = read(p[0], inbuf, MSGSIZE)) > 0) 
-			printf("% s\n", inbuf); 
+			printf("Message from parent:% s\n", inbuf); 
 		if (nbytes != 0) 
 			exit(2); 
 		printf("Finished reading in Child Process\n");
