d5ae6c37bb79664bbba35ab1f4e9c1ab1a2bfc05
tanmai-h <vvijaytanmai@gmail.com>
2019-08-16 09:22:01 +0530

    NTC Lab 3 fixes

--- 5th Semester/IT462 NTC/Lab 3/171IT202_IT462_P3.py
+++ 5th Semester/IT462 NTC/Lab 3/171IT202_IT462_P3.py
@@ -44,17 +44,25 @@ def check_if_square(n):
 
 def difference_square_factorization(k,n):
     kN = k*n 
-    b = 1
+    b = 0
     lines = []
-    # print(check_if_square(16))
-    while(True):
+    flag = 1
+
+    while(b<=int(n/2)):
         b_2 = b*b
         if check_if_square(kN + b_2):
+            flag = 0
             break
         b += 1
+    
+    if(flag):
+        lines.append("Cannot determine a and b (where b <= (n/2)) such that (k*n) + (b^2) == (a^2). Terminating")
+        file_ops(lines)
+        return
+
     a = int(math.sqrt(kN + b_2))
 
-    lines.append("Values of a and b such that (k*n) * (b^2) == (a^2):")
+    lines.append("Values of a and b such that (k*n) + (b^2) == (a^2):")
     lines.append("a: " + str(a))
     lines.append("b: " + str(b))
     
@@ -78,7 +86,7 @@ def difference_square_factorization(k,n):
     else:
         lines.append("Prime factors of gcd(n,a+b): N/A")
 
-    prime_factors = prime_factors_1 + prime_factors_2
+    prime_factors = list(set(prime_factors_1 + prime_factors_2))
     prime_factors.sort()
 
     lines.append("Prime factors of n using Difference of Squares method: " + ','.join([str(f) for f in prime_factors]))
@@ -101,15 +109,11 @@ def main():
         file_ops(lines)
         return 0
     if (n == 1):
-        lines.append((str(n) + " is not prime."))
+        lines.append((str(n) + " does not have any prime factors."))
         file_ops(lines)
         return 0
     if (n == 2):
-        lines.append((str(n) + " is prime."))
-        file_ops(lines)
-        return 0
-    if (n > 2) and (n%2 == 0):
-        lines.append((str(n) + " is even and greater than 2, and thus not prime."))
+        lines.append((str(n) + " is the only prime factor of itself."))
         file_ops(lines)
         return 0
     
