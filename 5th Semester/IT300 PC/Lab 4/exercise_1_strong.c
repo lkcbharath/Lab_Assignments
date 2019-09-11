4278a4654bdb81dc96e2d69ef047ad009544d873
student <student@itadmin.nitk.ac.in>
2019-09-11 15:06:01 +0530

    PC Lab 4 fixes 1

--- 5th Semester/IT300 PC/Lab 4/exercise_1_strong.c
+++ 5th Semester/IT300 PC/Lab 4/exercise_1_strong.c
@@ -1,8 +1,9 @@
 /*
 **  PROGRAM: Matrix Multiplicatin
 **
-**  PURPOSE: This is a simple program to calculate the nth Fibonacci number. 
-**           Each number in the Fibonacci series is the sum of the two previous numbers before it.
+**  PURPOSE: This is a simple program to multiply two matrices. 
+**           It contains both serial and parallel versions.
+**           
 **			 The first two numbers in the series are 0 and 1.
 **           Example of the first 10 numbers in the Fibonacci series:
 **
@@ -12,32 +13,62 @@
 **           If n goes below 2, then we return n.
 **           Else, we return the sum of the values returned by the functions taking n-1 and n-2 as input.
 **
-**  USAGE:   Run the program and enter an integer in the command line when prompted.
+**  USAGE:   Run the program and enter an integer in the command line when prompted, where the integer is the size of the array.
 **
-**  HISTORY: Written by Bharath Adikar, Aug 2019
+**  HISTORY: Written by Bharath Adikar, Sep 2019
 */
 #include <omp.h>
 #include <stdio.h>
-#define N 800
+#define N 500
 
 int main()
 {
     int m = N,n = N,p = N,q = N,i,j,k;
     int a[N][N],b[N][N],c[N][N];
-    double start_time, end_time;
+    double start_time_s,time_taken_s,start_time_p, time_taken_p;
+    FILE *fp;
+    fp = fopen ("output.txt","w");
 
     // Some initial values
+    start_time_s = omp_get_wtime();
+
     for(i=0;i<N;++i) {
         for(j = 0;j < N;j++) {
             b[i][j]=i;
             c[i][j]=j;
         }
     }
+    time_taken_s = omp_get_wtime() - start_time_s;
+
+    printf("Serialised part = %lf s\n",time_taken_s);
+
+    start_time_p = omp_get_wtime();
+    for (i=0;i<m;++i) {
+        for (j=0;j<n;++j) {
+            a[i][j]=0;
+            for (k=0;k<p;++k)
+                a[i][j] += b[i][k]*c[k][j];
+        }
+    }
+    time_taken_p = omp_get_wtime() - start_time_p;
+
+    printf("Time taken for serial matrix multiplication = %lf s\n",time_taken_p);
+    
 
     for(int itr=1;itr<=16;++itr){
         omp_set_num_threads(itr);
 
-        start_time = omp_get_wtime();
+        start_time_s = omp_get_wtime();
+        for (i=0;i<m;++i) {
+            for (j=0;j<n;++j) {
+                a[i][j]=0;
+                for (k=0;k<p;++k)
+                    a[i][j] += b[i][k]*c[k][j];
+            }
+        }
+        time_taken_s = omp_get_wtime() - start_time_s;
+
+        start_time_p = omp_get_wtime();
 
         #pragma omp parallel shared(a,b,c) private(i,j,k) 
         {
@@ -51,12 +82,12 @@ int main()
             }
         }
         
-        end_time = omp_get_wtime();
+        time_taken_p = omp_get_wtime()-start_time_p;
 
-        printf("Time taken for matrix multiplication with %d threads = %lf s\n",itr,end_time - start_time);
-        
+        printf("Time taken for parallel matrix multiplication with %d threads = %lf s\n",itr,time_taken_p);
+        fprintf (fp, "This is line %d\n",itr + 1);
     }
     
-
+    fclose(fp);
     return 0;
 }
