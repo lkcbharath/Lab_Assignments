90758d0cc49ea15d7b9e3f6d4fef5b740e67af21
student <student@itadmin.nitk.ac.in>
2019-10-01 14:26:03 +0530

    DBMS Lab 4 small change 3

--- 5th Semester/IT3xx DBMS/Lab 4/queries.sql
+++ 5th Semester/IT3xx DBMS/Lab 4/queries.sql
@@ -1,4 +1,4 @@
-SELECT pa.Name AS, ph.Name FROM Patient pa, PhysicianE ph WHERE pa.Pcp = ph.EmployeeID and ph.EmployeeID NOT IN (SELECT Head FROM Department);
+SELECT pa.Name AS Patient, ph.Name as Physician FROM Patient pa, PhysicianE ph WHERE pa.Pcp = ph.EmployeeID and ph.EmployeeID NOT IN (SELECT Head FROM Department);
 
 SELECT pa.Name FROM Patient pa, Appointment ap, Nurse nu WHERE pa.Ssn = ap.Patient AND ap.Prepnurse = nu.EmployeeID AND nu.registered = 1 AND pa.Pcp = ap.Physician GROUP BY pa.Name HAVING (COUNT(*) > 1);
 
@@ -6,6 +6,4 @@ SELECT pa.Name FROM Patient pa, Procedures pr, Undergoes un WHERE pa.Ssn = un.Pa
 
 SELECT ph.Name AS Physician, ph.Position, pr.Name AS 'Procedure Name', un.DateUndergoes AS 'Date Of Procedure', pa.Name AS Patient FROM PhysicianE ph, Procedures pr, Undergoes un, Patient pa, Trained_In tr WHERE un.DateUndergoes > tr.CertificationExpires AND ph.EmployeeID = un.Physician AND ph.EmployeeID = tr.Physician AND un.Procedures = pr.Code AND un.Patient = pa.Ssn;
 
-SELECT DISTINCT ph.Name FROM PhysicianE ph, Undergoes un, Procedures pr, Trained_In ti WHERE ph.EmployeeID = un.Physician AND ph.EmployeeID = ti.Physician AND un.Procedures = pr.Code AND (ti.Treatment != pr.Code);
-
-SELECT DISTINCT ph.Name FROM PhysicianE ph, Undergoes un, Trained_In ti WHERE ph.EmployeeID = un.Physician AND ph.EmployeeID = ti.Physician AND (un.DateUndergoes > ti.CertificationExpires OR un.Procedures NOT IN (SELECT Treatment FROM Trained_In WHERE Physician = ph.EmployeeID));
+SELECT DISTINCT ph.Name, un.Procedures FROM PhysicianE ph, Undergoes un, Trained_In ti WHERE ph.EmployeeID = un.Physician AND ph.EmployeeID = ti.Physician AND (un.DateUndergoes > ti.CertificationExpires OR un.Procedures NOT IN (SELECT Treatment FROM Trained_In WHERE Physician = ph.EmployeeID));
