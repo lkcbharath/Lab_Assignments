d923fcb0477e133f1e08e205780e75bddbe0596e
Bharath Adikar <lkcbharath@gmail.com>
2020-02-23 23:18:44 +0530

    HCI Lab 1 + DWDM Lab 1, 2 final

--- 6th Semester/IT414 DWDM/Lab 1/apriori.py
+++ 6th Semester/IT414 DWDM/Lab 1/apriori.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import pandas as pd
 from collections import defaultdict
-from itertools import combinations, product
+from itertools import combinations, product, permutations
 
 def custom_csv_import(filename,header_):
     
@@ -12,12 +12,12 @@ def custom_csv_import(filename,header_):
     with open(data_file, 'r') as temp_f:
         lines = temp_f.readlines()
         for l in lines:
-            column_count = len(l.split(data_file_delimiter)) + 1
+            column_count = len(l.split(data_file_delimiter))
             largest_column_count = column_count if largest_column_count < column_count else largest_column_count
 
     temp_f.close()
 
-    column_names = [i for i in range(0, largest_column_count)]
+    column_names = [int(i) for i in range(0, largest_column_count)]
 
     df = pd.read_csv(data_file, header=header_, delimiter=data_file_delimiter, names=column_names)
     return df.values
@@ -28,7 +28,8 @@ def self_join(keys,k):
         c_comb = set()
         for i in range(len(keys)):
             for j in range(i+1,len(keys)):
-                c_comb_item = tuple(set(keys[i] + keys[j]))
+                c_comb_item = tuple(set(tuple(keys[i]) + tuple(keys[j])))
+
                 
                 if len(c_comb_item) == k:
                     c_comb.add(c_comb_item)
@@ -37,8 +38,10 @@ def self_join(keys,k):
         # c_comb = [''.join(x) for x in product(keys, keys)]
         # print(c_comb)
         c_comb = set(tuple(sorted(l)) for l in c_comb if l[0] != l[1])
+        # print(c_comb)
     else:
-        c_comb = list(combinations(keys, k))
+        keys_ = [min(key) for key in keys]
+        c_comb = list(combinations(keys_, k))
 
     return c_comb
 
@@ -57,10 +60,11 @@ def generate_cand_set(ds_entries,c_comb,comb_count):
         keys = set_intersect(c_comb, ds_entry_comb)
 
         for key in keys:
-            if cand[key] == None:
-                cand[key] = 1
+            key_ = frozenset(key)
+            if cand[key_] == None:
+                cand[key_] = 1
             else:
-                cand[key] += 1
+                cand[key_] += 1
             
     return cand
 
@@ -69,15 +73,39 @@ def compare_sup_count(freq_set, min_sup_count):
                     if (val >= min_sup_count) }
     return new_dict
 
+def assoc_rule_mining(item_set,min_conf):
+    print('Association Rule Mining and Confidence:')
+    k = len(item_set)
+    for i in range(1,k):
+        freq_set = item_set[i]
+        keys = [list(i) for i in freq_set.keys()]
+        for key in keys:
+            perm_keys = list(permutations(key,len(key)))
+
+            for perm_key in perm_keys:
+                for i in range(1,len(perm_key)):
+                    key_ = frozenset(perm_key)
+                    if_1 = frozenset(perm_key[:i])
+                    then_1 = frozenset(perm_key[i:])
+                    sup_if_1 = item_set[len(if_1)-1][if_1]
+                    sup_key = item_set[len(key_)-1][key_]
+                    conf_value = sup_key/sup_if_1
+                    strong = ''
+                    if conf_value > min_conf:
+                        strong = ', Strong Rule'
+                    rule = 'Rule: ' + '^'.join(if_1) + ' -> ' + '^'.join(then_1) + ', Confidence = ' + str(round(conf_value,4)*100) + '%' + strong
+                    print(rule)
+
 def main():
 
     filename = 'test_dataset_1.csv'
-    header = None
+    header = 0
 
     # filename = 'retail_dataset.csv'
     # header = 'infer'
     
     ds_entries = custom_csv_import(filename, header)
+    print(ds_entries)
 
     # L1,L2
     item_set = []
@@ -96,10 +124,10 @@ def main():
     c1.pop(np.nan, None)
 
     l1 = compare_sup_count(c1, min_sup_count)
+    l1 = dict((frozenset([k]),v) for k,v in l1.items())
     item_set.append(l1)
 
     no_of_items = len(c1)
-    # print(c1)
 
     for k in range(2,no_of_items):
         c_comb = self_join(list(item_set[-1].keys()), k)
@@ -107,10 +135,10 @@ def main():
         lx = compare_sup_count(cand, min_sup_count)
         if lx != {}:
             item_set.append(lx)
-        
-    print(item_set)
+            
+    conf = 0.75
 
-
+    assoc_rule_mining(item_set,conf)
 
 
 if __name__ == '__main__':
--- 6th Semester/IT414 DWDM/Lab 2/fptree.py
+++ 6th Semester/IT414 DWDM/Lab 2/fptree.py
@@ -1,17 +1,23 @@
 import numpy as np 
 import pandas as pd
 from collections import defaultdict
+from itertools import permutations,chain,combinations
 
 class Node:
     
-    def __init__(self, item_name, item_count):
-        self.item = [item_name, item_count]
+    def __init__(self, item_name, item_count, item_id):
+        self.item = [item_name, item_count, item_id]
+        self.parent = None
         self.children = defaultdict(lambda: None)
 
 class FP_Tree:
 
     def __init__(self):
-        self.root = Node(None,None)
+        self.global_id_count = 0
+        self.root = Node(None,None,0)
+        self.cond_pattern_base = defaultdict(lambda: None)
+        self.cond_fp_tree = defaultdict(lambda: None)
+        self.freq_itemset_gen = defaultdict(lambda: None)
 
     def insert(self,node,transaction):
 
@@ -24,11 +30,107 @@ class FP_Tree:
         if node_ != None:
             node_.item[1] += 1
         else:
-            node_ = Node(item, 1)
+            self.global_id_count += 1
+            node_ = Node(item, 1, self.global_id_count)
+            node_.parent = node
             node.children[item] = node_
         
         self.insert(node_, transaction[1:])
+    
+    def find_pattern_base(self,node,items,branch_no):
+        items_ = items[::]
 
+        if node.item[0] != None:
+            items_ += [node.item[0]]
+
+        # Non-root entries only
+        if node.item[0] != None and len(items) > 0:
+
+            if node.item[0] not in self.cond_pattern_base.keys():
+                self.cond_pattern_base[node.item[0]] = defaultdict(lambda: None)
+            
+            pattern_base_item = self.cond_pattern_base[node.item[0]]
+
+            pattern_base = frozenset(items)
+
+            if pattern_base in pattern_base_item.keys():
+                pattern_base_item[pattern_base][0] += node.item[1]
+            else:
+                pattern_base_item[pattern_base] = [node.item[1], branch_no]
+
+        if node.item[0] == None:
+            i = 0
+            for node_key, node_ref in node.children.items():
+                self.find_pattern_base(node_ref, items_, i)
+                i += 1
+        else:
+            for node_key, node_ref in node.children.items():
+                self.find_pattern_base(node_ref,items_, branch_no)
+    
+    def get_cond_fp_tree(self,min_sup):
+        keys = list(self.cond_pattern_base.keys())
+        for pb_key in keys:
+            # branches = set([value[1] for value in self.cond_pattern_base[key].values()])
+            branches = defaultdict(lambda: None)
+            # branch_ids = set([value[1] for value in self.cond_pattern_base[key].values()])
+            # print(branches)
+            for key, value in self.cond_pattern_base[pb_key].items():
+                # print(key,value,pb_key)
+                branch_id = value[1]
+                if branches[branch_id] != None:
+                    branches[branch_id].add((key,value[0]))
+                else:
+                    branches[branch_id] = set()
+                    branches[branch_id].add((key,value[0]))
+                # branches[value[1]] = []
+
+            # self.cond_fp_tree[min(key)] = branches
+            self.cond_fp_tree[pb_key] = set()
+            for branch_id, branch in branches.items():
+                branch_dict = defaultdict(lambda: None)
+                # print(pb_key, branch_id, branch, 'test')
+                for items in branch:
+                    # print(items)
+                    item_value = items[1]
+                    for item_key in items[0]:
+                        # print(item_key)
+                        if branch_dict[item_key] != None:
+                            branch_dict[item_key] += item_value
+                        else:
+                            branch_dict[item_key] = item_value
+                # print(branch_dict)
+                branch_dict = {k: v for k,v in branch_dict.items() if v >= min_sup}
+                # print(pb_key, branch_id, branch_dict)
+                # print(pb_key)
+                self.cond_fp_tree[pb_key].add(tuple(branch_dict.items()))
+
+    
+    def gen_freq_item_set(self):
+        for node_key, node_cond_fp_tree in self.cond_fp_tree.items():
+            # items = list(node_cond_fp_tree.keys()) + [node_key]
+            # items = [' '.join(tups) for tups in node_cond_fp_tree]
+            # print(node_cond_fp_tree)
+            items = list(set(chain(*node_cond_fp_tree)))
+            dict_items = dict(items)
+            item_keys = dict_items.keys()
+            # print(node_key, item_keys)
+            self.freq_itemset_gen[node_key] = set()
+
+            # print(dict_items)
+
+            for i in range(1,len(item_keys)+1):
+                # print(item_keys)
+                perm_keys = list(combinations(item_keys,i))
+
+                # print(perm_keys)
+                for perm_key in perm_keys:
+                    min_dict = {k:v for k,v in dict_items.items() if k in perm_key}
+                    min_value = min(min_dict.values())
+                    # print(perm_key,min_value)
+                    item_to_add = tuple(list(perm_key) + [node_key])
+                    tup_to_add = (item_to_add,min_value)
+
+                    self.freq_itemset_gen[node_key].add(tup_to_add)
 
 def custom_csv_import(filename, header_):
 
@@ -39,7 +141,7 @@ def custom_csv_import(filename, header_):
     with open(data_file, 'r') as temp_f:
         lines = temp_f.readlines()
         for l in lines:
-            column_count = len(l.split(data_file_delimiter)) + 1
+            column_count = len(l.split(data_file_delimiter))
             largest_column_count = column_count if largest_column_count < column_count else largest_column_count
 
     temp_f.close()
@@ -54,7 +156,7 @@ def custom_csv_import(filename, header_):
 def main():
 
     filename = 'test_dataset_1.csv'
-    header = None
+    header = 0
 
     # filename = 'retail_dataset.csv'
     # header = 'infer'
@@ -74,9 +176,7 @@ def main():
 
     transactions = []
 
-
     tree = FP_Tree()
-
 
     for ds_entry in ds_entries:
         transaction = [item for item in ds_entry if not (pd.isnull(item))]
@@ -84,12 +184,15 @@ def main():
         transactions.append(transaction)
         tree.insert(tree.root,transaction)
 
-    # print(transactions)
+    tree.find_pattern_base(tree.root,list(),0)
 
-    # tree.insert(tree.root, transactions[0])
-    print(tree.root.children[transaction[0]].children['I1'].item)
+    sup = 2
+    tree.get_cond_fp_tree(sup)
 
-    
+    tree.gen_freq_item_set()
+
+    for key, value in tree.freq_itemset_gen.items():
+        print(key,value)
 
 if __name__ == '__main__':
     main()
